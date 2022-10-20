bf77cffb192ce0208ed1a1c46eef36d89a331406
RowanSC123 <113708932+RowanSC123@users.noreply.github.com>
2022-10-19 20:30:05 -0700

    Update and rename Chat_Bot_2.0.txt to Bot2.java

--- Bot2.java
+++ Bot2.java
@@ -1,5 +1,3 @@
-Bot2.java:
----------
 public class Bot2 {
 
     private String name;
@@ -61,34 +59,3 @@ public class Bot2 {
     }
 
 }
-
-Bot2Tester.java:
----------------
-import java.util.Scanner;
-
-public class Bot2Tester
-{
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        
-        System.out.print("Hello. What is your name? ");
-        String name = input.nextLine();
-        Bot2 obj = new Bot2(name);
-        obj.greeting();
-        
-        System.out.println("What is your favorite animal? ");
-        String animal = input.nextLine();
-        obj.favoriteAnimal(animal);
-        
-        System.out.println("Where do you live? ");
-        String location = input.nextLine();
-        obj.home(location);
-        
-        System.out.println("What is your favorite number? ");
-        int number = input.nextInt();
-        obj.favoriteNumber(number);
-        
-        obj.goodbye();
-    }
-}
-
